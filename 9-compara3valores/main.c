8b5f5f0d79c3db338c8cbf54451cd2b7384cd09e
nargotik <964518+nargotik@users.noreply.github.com>
2018-10-15 20:55:42 +0200

    .

---
+++ 9-compara3valores/main.c
@@ -0,0 +1,36 @@
+/*!
+ * @header      main.c
+ * @abstract    Compara 3 Valores
+ * @discussion  Compara 3 Valoes e mostra o maior ou diz que sao todos iguais.
+ * @author      Daniel Torres <964518+nargotik@users.noreply.github.com>
+ * @link        Daniel Torres GIT <https://github.com/nargotik>
+ * @ref         some reference
+ * @version     1.0 15 de Outubro de 2018
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+    int v1,v2,v3;
+
+    printf("Valores ?");
+    scanf("%d %d %d",&v1,&v2,&v3);
+    
+    if (v1==v2 && v2==v3) {
+        printf("São Iguais");
+    }
+    else if (v1>=v2 && v1>=v3) {
+        printf("V1 é o maior %d",v1);
+    } else if (v2 >= v3 && v2 >= v1) {
+        printf("V2 é o maior %d", v2);
+    } else {
+        printf("V3 é o maior %d", v3);
+    }
+    return (EXIT_SUCCESS);
+}
+
+
